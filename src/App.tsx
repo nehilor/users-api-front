8dbdaef6c4aea4a723b9c438852d28b95db5ee5d
Gabriel Villalobos <xnehilorx@gmail.com>
2023-06-03 18:23:14 -0600

    Updated login path

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ const App: React.FC = () => {
   return (
       <Router>
         <Routes>
+            <Route path="/" element={<Login/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/users" element={<Users/>}/>
         </Routes>
